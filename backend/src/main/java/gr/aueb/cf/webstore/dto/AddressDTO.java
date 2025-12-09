b18bcd6353c01034e8c6821ddde04f3dc9b25fbd
Ioannis <ioannis.dim@outlook.com>
2025-12-09 16:19:34 +0200

    feat: update some fields in AddressDTO and PaymentRequestDTO

--- backend/src/main/java/gr/aueb/cf/webstore/dto/AddressDTO.java
+++ backend/src/main/java/gr/aueb/cf/webstore/dto/AddressDTO.java
@@ -1,9 +1,18 @@
 package gr.aueb.cf.webstore.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record AddressDTO(
+        @NotBlank(message = "Street is required")
         String street,
+
+        @NotBlank(message = "City is required")
         String city,
+
+        @NotBlank(message = "Zipcode is required")
         String zipcode,
+
+        @NotBlank(message = "Country is required")
         String country
 ) {}
 
--- backend/src/main/java/gr/aueb/cf/webstore/dto/PaymentRequestDTO.java
+++ backend/src/main/java/gr/aueb/cf/webstore/dto/PaymentRequestDTO.java
@@ -1,6 +1,18 @@
 package gr.aueb.cf.webstore.dto;
 
 import gr.aueb.cf.webstore.core.enums.PaymentMethod;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
-public record PaymentRequestDTO (@NotNull Long orderId, @NotNull PaymentMethod method, String cardNumber) {}
+public record PaymentRequestDTO (
+        @NotNull
+        Long orderId,
+
+        @NotNull
+        PaymentMethod method,
+
+        @NotBlank(message = "Card number is required")
+        @Pattern(regexp = "^[0-9]{16}$", message = "Card number must be 16 digits")
+        String cardNumber
+) {}
